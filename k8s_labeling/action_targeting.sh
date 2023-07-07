3438829822866a22e4419369cbde437d9254eb1b
Pro <ahtyrka@gmail.com>
2023-07-07 18:10:52 +0300

    label: update name of variable for parsing

--- k8s_labeling/action_targeting.sh
+++ k8s_labeling/action_targeting.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# k get configmap | while read line; do pattern=$(echo $line| cut -d " " -f1); if [[ $pattern == *lambda* ]]; then echo $pattern; fi ; done
+# k get configmap | while read line; do name=$(echo $line| cut -d " " -f1); if [[ $name == *lambda* ]]; then echo $name; fi ; done
 #######
-# add label to k8s resoutces with some pattern
+# add label to k8s resoutces with some name
 #######
 
 labelname="labelname=value"
@@ -10,14 +10,14 @@ count=0
 command_input="targettext_sample.txt" # result of kubectl get configmap
 
 cat $command_input | while read line; do
-	pattern=$(echo $line| cut -d " " -f1);
-	if [[ $pattern == *lambda* ]] || [[ $pattern == *thanks* ]]; then
-		echo $pattern;
+	name=$(echo $line| cut -d " " -f1);
+	if [[ $name == *lambda* ]] || [[ $name == *thanks* ]]; then
+		echo $name;
 		count=$((count+1));
 		echo $count;
 		if [[ $count -le 1 ]]; then #to how many lines aplly the next command
-			echo "kubectl label configmap $pattern $labelname"
-			#kubectl label configmap $pattern $labelname
+			echo "kubectl label configmap $name $labelname"
+			#kubectl label configmap $name $labelname
 		fi
 	fi;
 done
