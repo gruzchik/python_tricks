77dc05a46dc5ab487181f13787ec9462014979d6
Pro <ahtyrka@gmail.com>
2023-07-15 16:34:28 +0300

    k8s_labeling: gruzchik trailing-whitespace and uppercase fix

--- k8s_labeling/action_targetting_gruzchik.py
+++ k8s_labeling/action_targetting_gruzchik.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 
 # settings
-labelname="labelname=value"
-command_input="targettext_sample.txt" # result of kubectl get configmap
-cycle_quantity="3"
+LABELNAME="labelname=value"
+COMMAND_INPUT="targettext_sample.txt" # result of kubectl get configmap
+CYCLE_QUANTITY="3"
 
 
-bash_command="kubectl get configmap"
+BASH_COMMAND="kubectl get configmap"
 
 # Opening file
-input_file = open(command_input, 'r')
-count = 0
+input_file = open(COMMAND_INPUT, 'r')
+COUNT = 0
 
 # Using for loop
 print("Using for loop")
 for line in input_file:
     name=line.split()[0] # name of configmap
     if "lambda" in name or "thanks" in name:
-        count += 1
-        if int(cycle_quantity) >= count:
-            print("kubectl configmap label " +name+ " " +labelname)
+        COUNT += 1
+        if int(CYCLE_QUANTITY) >= COUNT:
+            print("kubectl configmap label " +name+ " " +LABELNAME)
         print(name)
-        print(count)
+        print(COUNT)
 
 # Closing files
 input_file.close()
