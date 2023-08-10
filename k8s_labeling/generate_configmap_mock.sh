7f91989470807045ab66f4dc1561068a63fe5d66
Pro <ahtyrka@gmail.com>
2023-08-11 02:04:03 +0300

    k8s_labeling: update generate_configmap_mock.sh path in CI new debug 1

--- k8s_labeling/generate_configmap_mock.sh
+++ k8s_labeling/generate_configmap_mock.sh
@@ -12,7 +12,7 @@ echo "someacc-cfg        1      422d"
 echo "newacc-cfg        1      422d"
 
 for ((n=0;n<${LAMBDA_FIX_QUANTITY};n++)); do
-	RANDOM_HASH_L=$(cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 10 | head -n 1)
+	RANDOM_HASH_L=$(head -c 100 /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 10 | head -n 1)
 	RANDOM_DAYS_L=$((1 + $RANDOM % 300))	
 	echo "lambda-fix-${RANDOM_HASH_L}   1      ${RANDOM_DAYS_L}d"
 	# echo "lambda-fix-11111   1      ${RANDOM_DAYS_L}d"
