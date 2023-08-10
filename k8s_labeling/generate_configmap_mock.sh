519171ba85a41ebf533997aee6e333b528c789d0
Pro <ahtyrka@gmail.com>
2023-08-11 02:09:10 +0300

    k8s_labeling: update generate_configmap_mock.sh path in CI new debug final 1

--- k8s_labeling/generate_configmap_mock.sh
+++ k8s_labeling/generate_configmap_mock.sh
@@ -15,12 +15,10 @@ for ((n=0;n<${LAMBDA_FIX_QUANTITY};n++)); do
 	RANDOM_HASH_L=$(head -c 100 /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 10 | head -n 1)
 	RANDOM_DAYS_L=$((1 + $RANDOM % 300))	
 	echo "lambda-fix-${RANDOM_HASH_L}   1      ${RANDOM_DAYS_L}d"
-	# echo "lambda-fix-11111   1      ${RANDOM_DAYS_L}d"
 done
 
 for ((n=0;n<${THANKS_QUANTITY};n++)); do
-    #     RANDOM_HASH_T=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-        RANDOM_DAYS_T=$((1 + $RANDOM % 300))	
-	# echo "thanks-${RANDOM_HASH_T}          1      ${RANDOM_DAYS_T}d"
-	echo "thanks-222          1      ${RANDOM_DAYS_T}d"
+    RANDOM_HASH_T=$(head -c 100 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+    RANDOM_DAYS_T=$((1 + $RANDOM % 300))	
+	echo "thanks-${RANDOM_HASH_T}          1      ${RANDOM_DAYS_T}d"
 done
