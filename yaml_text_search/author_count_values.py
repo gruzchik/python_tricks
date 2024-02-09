e5abdcf15c9d76e60eb916f6e0edca0b0401653d
Pro <ahtyrka@gmail.com>
2024-02-09 07:01:38 +0200

    yaml_text_search: delete unused code

--- yaml_text_search/author_count_values.py
+++ yaml_text_search/author_count_values.py
@@ -4,32 +4,9 @@
 
 import yaml
 
-# settings
+# settings file
 with open("config_depl_files.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-
-
-# count=0
-# with open(VALUE_LIST_ZERO,'r') as file:
-#     for i in file:
-#         if "value:" in i:
-#             #print("check.."+i+"\n")
-#             i=i.strip()
-#             sample=i.split(": ")
-#             #print(sample[1])
-#             with open(CHECK_LIST1,'r') as file1:
-#                 for j in file1:
-#                     if "key" in j:
-#                         # count=count+1
-#                         # print(count)
-#                         #print("checking.."+j+"\n")
-#                         j=j.strip()
-#                         element=j.split(": ")
-#                         #print(element[1])
-#                         if sample[1] == element[1]:
-#                             count=count+1
-#                             print(f'value {count}: {element[1]}')
-#                             break
 
 def retrieve_setting_values() -> list:
     ''' retrieve values from the settings file '''
